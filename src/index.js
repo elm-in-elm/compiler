2b7e92fd3c9e348c697da33929f2152a1fdcaf57
Martin Janiczek <martin@janiczek.cz>
2019-01-11 03:32:13 +0100

    Requires

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-// needs the Elm code to be compiled
-const {Elm} = require('../build/elm.js');
-const {registerPort} = require('./utils.js');
-const fs = require('fs');
-const fsPromises = fs.promises;
+/* TODO Is `require()`ing dependencies where we need them better than
+        all at the top? Measure! It makes the code look very unusual
+        so there should be a good reason for keeping it.
+*/
 
+// Async/await is nice! This means we need Node.JS v7.6 at minimum though.
 (async function(){
 
+  const fs = require('fs');
+  const fsPromises = fs.promises;
+
+  // TODO Don't hardcode the elm.json filepath.
   const elmJson = await fsPromises.readFile('test/elm.json', {encoding: 'utf8'});
+
+  // The following line needs the Elm code to be compiled! See `Makefile`.
+  // No Webpack around here!
+  const {Elm} = require('../build/elm.js');
 
   const app = Elm.Main.init({
     flags: {
@@ -14,6 +22,8 @@ const fsPromises = fs.promises;
       elmJson,
     }
   });
+
+  const {registerPort} = require('./utils.js');
 
   registerPort(app, 'stdout', string => process.stdout.write(string));
   registerPort(app, 'stderr', string => process.stderr.write(string));
