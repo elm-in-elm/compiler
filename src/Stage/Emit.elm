c0209eefddcc301284dde3660c603178631411b5
Martin Janiczek <martin@janiczek.cz>
2019-02-14 01:32:16 +0100

    Emit everything, not just the Main.main

--- src/Stage/Emit.elm
+++ src/Stage/Emit.elm
@@ -21,21 +21,19 @@ import Dict.Any
 import Error exposing (EmitError(..), Error(..))
 
 
-{-| TODO emit everything from every module, not just Main's main
-TODO check for dependencies and emit in dependency order
+{-| TODO check for dependencies and emit in dependency order
 TODO dead code elimination
 -}
 emit : Project Backend.Expr -> Result Error ProjectToEmit
 emit project =
     project.program
-        |> Dict.Any.get project.mainModuleName
-        |> Result.fromMaybe (EmitError MainModuleNotFound)
-        |> Result.andThen
-            (.topLevelDeclarations
-                >> Dict.Any.get (VarName "main")
-                >> Result.fromMaybe (EmitError MainDeclarationNotFound)
-            )
-        |> Result.map (emitTopLevelDeclaration >> FileContents >> ProjectToEmit)
+        |> Dict.Any.values
+        |> List.concatMap (.topLevelDeclarations >> Dict.Any.values)
+        |> List.map emitTopLevelDeclaration
+        |> String.join "\n"
+        |> FileContents
+        |> ProjectToEmit
+        |> Ok
 
 
 emitTopLevelDeclaration : TopLevelDeclaration Backend.Expr -> String
