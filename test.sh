ce9e255c2827f15a4c30386e54c5d733593261c2
Martin Janiczek <martin@janiczek.cz>
2019-03-30 23:55:42 +0100

    License

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+COLOR_OFF="\e[0m";
+DIM="\e[2m";
+
+function run {
+  clear;
+  tput reset;
+
+  echo -en "${DIM}";
+  date -R;
+  echo -en "${COLOR_OFF}";
+
+  yarn elm-test;
+}
+
+run;
+
+inotifywait -mqr -e close_write --format '%w %e %f' src tests | while read DIR EVENT FILE; do
+  run;
+done;
