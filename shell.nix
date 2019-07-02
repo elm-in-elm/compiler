8d653109b95d13bd84fdd15c524cc11752e8c9a0
Martin Janiczek <martin@janiczek.cz>
2019-07-02 10:45:45 +0200

    use explicit nixpkgs revision to circumvent the need for unstable

--- shell.nix
+++ shell.nix
@@ -1,10 +1,12 @@
-# as of 2019-06-30, this must be run with unstable channel
-# nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs 
-# nix-channel --update
-
-with import <nixpkgs>{};
-stdenv.mkDerivation rec {
-    name = "elm-in-elm";
+let
+  nixpkgs = (import <nixpkgs> {}).fetchFromGitHub {
+    owner = "nixOS";
+    repo = "nixpkgs-channels";
+    rev = "73392e79aa62e406683d6a732eb4f4101f4732be";
+    sha256 = "049fq37sxdn5iis7ni407j2jsfrxbb41pgv5zawi02qj47f74az9";
+  };
+in with import nixpkgs {};
+mkShell {
     buildInputs = with elmPackages; [
         nodejs-10_x
         elm elm-test elm-format elm-analyse
