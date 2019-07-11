f490ff968953bb0e59c5a2707e7d66f0e483e3ea
Maxime DANTEC <hello@warry.fr>
2019-07-11 18:42:06 +0200

    better tests

--- tests/InferTypesTest.elm
+++ tests/InferTypesTest.elm
@@ -1,10 +1,11 @@
-module InferTypesTest exposing (typeInference)
+module InferTypesTest exposing (typeInference, typeToString)
 
 import AST.Canonical as Canonical
 import AST.Common.Literal exposing (Literal(..))
 import AST.Common.Type as Type exposing (Type)
 import AST.Typed as Typed
 import Common
+import Common.Types as Types
 import Dict.Any
 import Error exposing (TypeError)
 import Expect exposing (Expectation)
@@ -47,6 +48,55 @@ typeInference =
                 , ( "more items with different types"
                   , Canonical.List [ Canonical.Literal (Bool True), Canonical.Literal (String "two"), Canonical.Literal (Int 3) ]
                   , Err (Error.TypeMismatch Type.Bool Type.String)
+                  )
+                ]
+              )
+            ]
+        )
+
+
+typeToString : Test
+typeToString =
+    let
+        runSection ( description, tests ) =
+            describe description
+                (List.map runTest tests)
+
+        runTest ( description, input, output ) =
+            test description <|
+                \() ->
+                    Type.toString input
+                        |> Expect.equal output
+    in
+    describe "Type.toString"
+        (List.map runSection
+            [ ( "list"
+              , [ ( "empty list"
+                  , Type.List (Type.Var 0)
+                  , "List a"
+                  )
+                , ( "one item in list"
+                  , Type.List Type.Bool
+                  , "List Bool"
+                  )
+                ]
+              )
+            , ( "lambda"
+              , [ ( "function with one param"
+                  , Type.Function (Type.Var 0) Type.Int
+                  , "a -> Int"
+                  )
+                , ( "function with two params"
+                  , Type.Function (Type.Var 0) (Type.Function (Type.Var 1) (Type.Var 1))
+                  , "a -> b -> b"
+                  )
+                , ( "function as params"
+                  , Type.Function (Type.Function (Type.Var 1) (Type.Var 1)) (Type.Var 0)
+                  , "(b -> b) -> a"
+                  )
+                , ( "list of functions"
+                  , Type.List (Type.Function (Type.Var 0) (Type.Var 0))
+                  , "List (a -> a)"
                   )
                 ]
               )
