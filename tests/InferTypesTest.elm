d24a29afc7890254bee0c2e46e972767e8005f98
Karol Marcjan <karol.marcjan@gmail.com>
2019-07-11 19:45:14 +0200

    InferTypeTest: Extract runSection with different type

--- tests/InferTypesTest.elm
+++ tests/InferTypesTest.elm
@@ -14,38 +14,36 @@ import Test exposing (Test, describe, test)
 
 typeInference : Test
 typeInference =
-    let
-        runSection ( description, tests ) =
-            describe description
-                (List.map runTest tests)
-    in
     describe "Stage.InferType"
-        (List.map runSection
-            [ ( "list"
-              , [ ( "empty list"
-                  , Canonical.List []
-                  , Ok ( Typed.List [], Type.List (Type.Var 1) )
-                  )
-                , ( "one item"
-                  , Canonical.List [ Canonical.Literal (Bool True) ]
-                  , Ok ( Typed.List [ ( Typed.Literal (Bool True), Type.Bool ) ], Type.List Type.Bool )
-                  )
-                , ( "more items"
-                  , Canonical.List [ Canonical.Literal (Int 1), Canonical.Literal (Int 2), Canonical.Literal (Int 3) ]
-                  , Ok ( Typed.List [ ( Typed.Literal (Int 1), Type.Int ), ( Typed.Literal (Int 2), Type.Int ), ( Typed.Literal (Int 3), Type.Int ) ], Type.List Type.Int )
-                  )
-                , ( "different types"
-                  , Canonical.List [ Canonical.Literal (Int 1), Canonical.Literal (String "two") ]
-                  , Err (Error.TypeMismatch Type.Int Type.String)
-                  )
-                , ( "more items with different types"
-                  , Canonical.List [ Canonical.Literal (Bool True), Canonical.Literal (String "two"), Canonical.Literal (Int 3) ]
-                  , Err (Error.TypeMismatch Type.Bool Type.String)
-                  )
-                ]
+        [ runSection "list"
+            [ ( "empty list"
+              , Canonical.List []
+              , Ok ( Typed.List [], Type.List (Type.Var 1) )
+              )
+            , ( "one item"
+              , Canonical.List [ Canonical.Literal (Bool True) ]
+              , Ok ( Typed.List [ ( Typed.Literal (Bool True), Type.Bool ) ], Type.List Type.Bool )
+              )
+            , ( "more items"
+              , Canonical.List [ Canonical.Literal (Int 1), Canonical.Literal (Int 2), Canonical.Literal (Int 3) ]
+              , Ok ( Typed.List [ ( Typed.Literal (Int 1), Type.Int ), ( Typed.Literal (Int 2), Type.Int ), ( Typed.Literal (Int 3), Type.Int ) ], Type.List Type.Int )
+              )
+            , ( "different types"
+              , Canonical.List [ Canonical.Literal (Int 1), Canonical.Literal (String "two") ]
+              , Err (Error.TypeMismatch Type.Int Type.String)
+              )
+            , ( "more items with different types"
+              , Canonical.List [ Canonical.Literal (Bool True), Canonical.Literal (String "two"), Canonical.Literal (Int 3) ]
+              , Err (Error.TypeMismatch Type.Bool Type.String)
               )
             ]
-        )
+        ]
+
+
+runSection : String -> List ( String, Canonical.Expr, Result Error.TypeError Typed.Expr ) -> Test
+runSection description tests =
+    describe description
+        (List.map runTest tests)
 
 
 runTest : ( String, Canonical.Expr, Result Error.TypeError Typed.Expr ) -> Test
