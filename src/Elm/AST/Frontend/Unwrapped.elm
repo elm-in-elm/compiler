4120fe7e397070b90a01473b1d838bc82f08074d
Martin Janiczek <martin@janiczek.cz>
2019-09-28 19:10:05 +0200

    Forgotten types 2

--- src/Elm/AST/Frontend/Unwrapped.elm
+++ src/Elm/AST/Frontend/Unwrapped.elm
@@ -2,6 +2,8 @@ module Elm.AST.Frontend.Unwrapped exposing (Expr(..))
 
 import Elm.AST.Common.Literal exposing (Literal)
 import Elm.Data.Binding exposing (Binding)
+import Elm.Data.ModuleName exposing (ModuleName)
+import Elm.Data.VarName exposing (VarName)
 
 
 {-| This only differs from AST.Frontend.Expr by recursing on itself instead of
@@ -9,12 +11,12 @@ on LocatedExpr. Handy for parser tests or when you don't need the location info!
 -}
 type Expr
     = Literal Literal
-    | Var { module_ : Maybe String, name : String }
-    | Argument String
+    | Var { module_ : Maybe ModuleName, name : VarName }
+    | Argument VarName
     | Plus Expr Expr
     | Cons Expr Expr
     | ListConcat Expr Expr
-    | Lambda { arguments : List String, body : Expr }
+    | Lambda { arguments : List VarName, body : Expr }
     | Call { fn : Expr, argument : Expr }
     | If { test : Expr, then_ : Expr, else_ : Expr }
     | Let { bindings : List (Binding Expr), body : Expr }
