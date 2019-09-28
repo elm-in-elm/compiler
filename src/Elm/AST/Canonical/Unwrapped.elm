3b3601304ee6d8744b9aaa9b8adef08317ba4a7e
Martin Janiczek <martin@janiczek.cz>
2019-09-28 19:08:05 +0200

    Forgotten types

--- src/Elm/AST/Canonical/Unwrapped.elm
+++ src/Elm/AST/Canonical/Unwrapped.elm
@@ -3,6 +3,8 @@ module Elm.AST.Canonical.Unwrapped exposing (Expr(..))
 import AssocList exposing (Dict)
 import Elm.AST.Common.Literal exposing (Literal)
 import Elm.Data.Binding exposing (Binding)
+import Elm.Data.ModuleName exposing (ModuleName)
+import Elm.Data.VarName exposing (VarName)
 
 
 {-| This only differs from AST.Canonical.Expr by recursing on itself instead of
@@ -10,14 +12,14 @@ on LocatedExpr. Handy for type inference fuzzers!
 -}
 type Expr
     = Literal Literal
-    | Var { module_ : String, name : String }
-    | Argument String
+    | Var { module_ : ModuleName, name : VarName }
+    | Argument VarName
     | Plus Expr Expr
     | Cons Expr Expr
-    | Lambda { argument : String, body : Expr }
+    | Lambda { argument : VarName, body : Expr }
     | Call { fn : Expr, argument : Expr }
     | If { test : Expr, then_ : Expr, else_ : Expr }
-    | Let { bindings : Dict String (Binding Expr), body : Expr }
+    | Let { bindings : Dict VarName (Binding Expr), body : Expr }
     | List (List Expr)
     | Unit
     | Tuple Expr Expr
