d9fe75e2f9a5e9637877537fc7d28ac37753aa55
Martin Janiczek <martin@janiczek.cz>
2020-06-14 23:11:06 +0200

    Better place

--- integration-tests/typecheck/type-annotation-qualified-type-err-not-same-as-inferred/test.test.js
+++ integration-tests/typecheck/type-annotation-qualified-type-err-not-same-as-inferred/test.test.js
@@ -2,7 +2,7 @@ const test = require('ava');
 const {exec} = require('../../setup');
 
 test.failing(
-    "desugar: qualified type in the type annotation is found but doesn't agree with the declaration",
+    "typecheck: qualified type in the type annotation is found but doesn't agree with the declaration",
     exec,
     __dirname,
     ["-m", "src/Main.elm"],
