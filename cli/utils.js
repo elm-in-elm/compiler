95529e5c308afa9e5761643b30848be2407edbc8
Martin Janiczek <martin@janiczek.cz>
2020-07-06 20:24:14 +0200

    Helper

--- cli/utils.js
+++ cli/utils.js
@@ -2,8 +2,8 @@ const registerPort = (app, portName, callback) => {
   if (app.ports && app.ports[portName]) {
     app.ports[portName].subscribe(callback);
   } else {
-    console.error(`Tried to register an Elm port callback but failed: ${portName}`);
-    process.exit(1);
+    console.error(`Tried to register an Elm port callback but failed: ${portName}. Most likely because the port is unused on the Elm side.`);
+    //process.exit(1);
   }
 };
 
