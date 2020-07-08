630e56a97416042fd7ec74700368bf5cca3e4e2c
Harry Sarson <harry.sarson@hotmail.co.uk>
2020-07-08 19:48:13 +0100

    refuse to parse operators unless they are indented

---
+++ integration-tests/parser/indentation/expr-needing-indentation/src/Main.elm
@@ -0,0 +1,6 @@
+module Main exposing (MyMaybe)
+
+
+main =
+  1
++ 7
---
+++ integration-tests/parser/indentation/expr-needing-indentation/test.test.js
@@ -0,0 +1,14 @@
+const test = require('ava');
+const {exec} = require('../../../setup');
+
+test(
+    'parser: indentation: expr needing indentation',
+    exec,
+    __dirname,
+    ["-m", "src/Main.elm"],
+    async (program, t) => {
+        const snapshot = await t.throwsAsync(program);
+        t.is(snapshot.code, 1);
+        return {snapshot};
+    }
+);
