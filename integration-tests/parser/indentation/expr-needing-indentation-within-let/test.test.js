38489c698ffa116682dd77ada8412ed0812c2378
Harry Sarson <harry.sarson@hotmail.co.uk>
2020-07-13 22:13:11 +0100

    check indentation within let block
    It is always great when a test "just works"!

---
+++ integration-tests/parser/indentation/expr-needing-indentation-within-let/test.test.js
@@ -0,0 +1,14 @@
+const test = require('ava');
+const {exec} = require('../../../setup');
+
+test(
+	'parser: indentation: expr needing indentation within let',
+	exec,
+	__dirname,
+	['-m', 'src/Main.elm'],
+	async (program, t) => {
+		const snapshot = await t.throwsAsync(program);
+		t.is(snapshot.code, 1);
+		await t.context.cliSnapshot(snapshot);
+	}
+);
