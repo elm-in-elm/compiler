a3a63e974964f0327099ab2be532ea5c27627857
Martin Janiczek <martin@janiczek.cz>
2021-07-13 20:07:51 +0200

    Update nix-shell

--- shell.nix
+++ shell.nix
@@ -1,14 +1,9 @@
-let
-  nixpkgs = (import <nixpkgs> {}).fetchFromGitHub {
-    owner = "nixOS";
-    repo = "nixpkgs-channels";
-    rev = "73392e79aa62e406683d6a732eb4f4101f4732be";
-    sha256 = "049fq37sxdn5iis7ni407j2jsfrxbb41pgv5zawi02qj47f74az9";
-  };
-in with import nixpkgs {};
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+with stdenv;
 mkShell {
-    buildInputs = with elmPackages; [
-        nodejs-10_x
-        elm elm-test elm-format elm-analyse
-    ];
+  buildInputs = with elmPackages; [ 
+    elm elm-format
+    nodejs-14_x 
+  ];
 }
